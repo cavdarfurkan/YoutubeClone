0a997397e3d0e3da9333e0ed1d662c015208e1e8
cavdarfurkan <m.furkancavdar@gmail.com>
2022-02-26 02:17:53 +0300

    Changed to SliverAppBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,38 +10,41 @@ class MainPage extends StatelessWidget {
     return MaterialApp(
       title: 'YouTube',
       home: Scaffold(
-        appBar: AppBar(
-          // toolbarHeight: 100,
-          backgroundColor: const Color.fromRGBO(32, 32, 32, 1),
-          elevation: 0,
-          leading: Padding(
-            padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-            child: Image.asset('assets/yt_logo.png'),
-          ),
-          leadingWidth: 95,
-          actions: [
-            IconButton(
-              onPressed: (){},
-              icon: const Icon(Icons.cast),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.notifications_none),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.search),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 0, 15, 0),
-              child: CircleAvatar(
-                radius: 15,
-                child: IconButton(
-                  onPressed: (){},
-                  icon: Image.asset('assets/avatar.jpg'),
-                ),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              backgroundColor: const Color.fromRGBO(32, 32, 32, 1),
+              elevation: 0,
+              leading: Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                child: Image.asset('assets/yt_logo.png'),
               ),
-            )
+              leadingWidth: 95,
+              actions: [
+                IconButton(
+                  onPressed: (){},
+                  icon: const Icon(Icons.cast),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.notifications_none),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: ClipOval(
+                      child: Image.asset('assets/avatar.jpg'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
