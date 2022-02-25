2f904d2be9f4d4ad0f496c82913d8bdc4c3100fe
cavdarfurkan <m.furkancavdar@gmail.com>
2022-02-26 02:24:45 +0300

    SliverAppBar moved to a seperate widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,49 +7,57 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'YouTube',
       home: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              floating: true,
-              backgroundColor: const Color.fromRGBO(32, 32, 32, 1),
-              elevation: 0,
-              leading: Padding(
-                padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-                child: Image.asset('assets/yt_logo.png'),
-              ),
-              leadingWidth: 95,
-              actions: [
-                IconButton(
-                  onPressed: (){},
-                  icon: const Icon(Icons.cast),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.notifications_none),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.search),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: ClipOval(
-                      child: Image.asset('assets/avatar.jpg'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+        body: CustomAppBar(),
       ),
     );
   }
 }
 
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          // expandedHeight: 100,
+          backgroundColor: const Color.fromRGBO(32, 32, 32, 1),
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+            child: Image.asset('assets/yt_logo.png'),
+          ),
+          leadingWidth: 95,
+          actions: [
+            IconButton(
+              onPressed: (){},
+              icon: const Icon(Icons.cast),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.notifications_none),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: IconButton(
+                onPressed: () {},
+                icon: ClipOval(
+                  child: Image.asset('assets/avatar.jpg'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
